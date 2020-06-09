ba364b37aa6f73809ce013c553385385f67cb3e3
Xfighter32 <austinhakanson2@gmail.com>
2020-06-09 08:29:26 -0600

    Clase para la indexacion de archivos.

---
+++ src/lucene/IndexFiles.java
@@ -0,0 +1,12 @@
+package lucene;
+
+import org.apache.lucene.index.IndexWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class IndexFiles {
+    static void indexDocs(final IndexWriter writer, Path path) throws IOException {
+        // Continuar con el video y el tutorial ...
+
+    }
+}
