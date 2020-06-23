dbcd9b00df4f5d26ebed4ccd7999e638e884c292
Xfighter32 <austinhakanson2@gmail.com>
2020-06-23 12:35:12 -0600

    Hace bien el ananlisis pero esta el siguiente problema: cannot change field "encab" from index options=DOCS to inconsistent index options=DOCS_AND_FREQS_AND_POSITIONS

--- src/Main.java
+++ src/Main.java
@@ -18,33 +18,23 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Lector lector = new Lector();
-        FileReader lectorStopWords;
-//         obtener stop words
-        try {
-//            Path rutaDirectorioStopWords = Paths.get(".","Proyecto-Lucene/stop words.txt");
-//            lectorStopWords= new FileReader(rutaDirectorioStopWords.toString());
-            // Se asignan los stopwords al analizador
-            StandardAnalyzer analizador = new StandardAnalyzer();
-            IndexWriterConfig configuracionIndice = new IndexWriterConfig(analizador);
-            Path rutaDirectorioIndice = Paths.get(".","Proyecto-Lucene/indices");
-            Directory directorioIndice = FSDirectory.open(rutaDirectorioIndice);
-            IndexWriter writer = new IndexWriter(directorioIndice,configuracionIndice);
 
-            Indexador indexador = new Indexador(writer);
-            try {
-                ArrayList<ArrayList<String>> archivos = lector.obtenerDocumentos();
-                for (ArrayList<String> htmls : archivos) {
-                    for (String html : htmls) {
-                        indexador.parsearHtml(html);
-                    }
+        Indexador indexador = new Indexador();
+        indexador.leerStopWords();
+        int cont = 0;
+        try {
+            ArrayList<ArrayList<String>> archivos = lector.obtenerDocumentos();
+            for (ArrayList<String> htmls : archivos) {
+                for (String html : htmls) {
+                    indexador.indexarContenidos(html);
+                    cont++;
                 }
             }
-            catch (IOException a) {
-                System.out.println("Hubo un problema al leer los archivos.");
-            }
+            indexador.writer.close();
+            System.out.println(cont + " indexados");
         }
-        catch (FileNotFoundException e) {
-            System.out.println("No se encontro el archivo de stop words");
+        catch (IOException a) {
+            System.out.println("Hubo un problema al leer los archivos.");
         }
     }
 }
--- src/lucene/Indexador.java
+++ src/lucene/Indexador.java
@@ -1,128 +1,147 @@
 package lucene;
 
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.snowball.SnowballFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.IndexWriter;
 
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.tika.parser.Parser;
-import org.apache.tika.parser.html.HtmlParser;
-import org.apache.tika.sax.ToHTMLContentHandler;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.tartarus.snowball.ext.SpanishStemmer;
+import ucar.ma2.Index;
 
 
 public class Indexador
 {
 
-	private IndexWriter writer;
+	public IndexWriter writer;
+	CharArraySet stopWords;
+	Analyzer analizador;
 	ArrayList<String> bodies, referencias, titulos, encabezados;
 
-	public Indexador(IndexWriter writer)
+	public Indexador()
     {
-        this.writer = writer;
+        this.writer = configurarIndexador();
         bodies = new ArrayList<String>();
         referencias = new ArrayList<String>();
         titulos = new ArrayList<String>();
         encabezados = new ArrayList<String>();
 	}
 
-	public void parsearHtml (String html)
-	{
-		org.jsoup.nodes.Document documento = Jsoup.parse(html);
-//		System.out.println(documento.body().text());
-
-//		documento.getElementsByTag("title");
-//		documento.getElementsByTag("h1");
-//		documento.getElementsByTag("h2");
-//		documento.getElementsByTag("h3");
-//		documento.getElementsByTag("h4");
-//		documento.getElementsByTag("h5");
-//		documento.getElementsByTag("h6");
-//		documento.getElementsByTag("a");
-
-		Elements referencias = documento.getElementsByTag("a");
-		for (Element ref : referencias){
-			System.out.println(ref.attr("abs:href"));
+	public void leerStopWords() throws IOException {
+		ArrayList<String> listaStopWords = new ArrayList<String>();
+		Path ubicacionStopWords = Paths.get(".","stop_words");
+		File archivoStopWords = new File(ubicacionStopWords.toString());
+		try (BufferedReader lector = new BufferedReader(new FileReader(archivoStopWords))) {
+			String stopWord;
+			while ((stopWord = lector.readLine()) != null){
+				listaStopWords.add(stopWord);
+			}
 		}
-
-
+		stopWords = new CharArraySet(listaStopWords,false);
 	}
-//	public void indexDocument(String html) throws Exception {
-//
-//		try {
-//			Document document = new Document();
-//			document.add(new Field());
-//			document.add(new Field("fulltext", fulltext));
-//			writer.addDocument(document);
-//		}
-//		finally {
-//			fulltext.close();
-//		}
+
+	private static void mostrarTokens(Analyzer analyzer,String text) throws IOException
+	{
+		TokenStream stream = analyzer.tokenStream(null,new StringReader(text));
+		CharTermAttribute caracter = stream.addAttribute(CharTermAttribute.class);
+		stream.reset();
+		while (stream.incrementToken()) {
+			System.out.print(" [" + caracter.toString()+ "] ");
+		}
+		stream.end();
+		stream.close();
+	}
+
+//	public TokenStream tokenStream(String fieldName, Reader reader) {
+//		return new SnowballFilter()new StopFilter(true,
+//				new LowerCaseTokenizer(reader),
+//				stopWords);
 //	}
-//
-//
-//		// obtener stop words
-//		FileReader lectorStopWords = new FileReader("stop words.txt");
-//		// Se asignan los stopwords al analizador
-//		StandardAnalyzer analizador = new StandardAnalyzer(lectorStopWords);
-//		//Se define el directorio donde se almacenaran los indices
-//		Path rutaDirectorioIndice = Paths.get(".","Proyecto-Lucene/indices");
-//		Directory directorioIndice = FSDirectory.open(rutaDirectorioIndice);
-//		// Se configura el indezador con el analizador
-//		IndexWriterConfig configuracionIndice = new IndexWriterConfig(analizador);
-//		// Se crea el indexador
-//		writer = new IndexWriter(directorioIndice,configuracionIndice);
-//	}
-//	public void close() throws CorruptIndexException, IOException
-//	{
-//		writer.close();
-//	}
-//	private Document getDocument(File file) throws IOException
-//	{
-//		Document document = new Document();
-//		//index file contents
-//		Field contentField = new Field();
-//		//index file name
-//		Field fileNameField = new Field(LuceneConstants.FILE_NAME,
-//			file.getName(),
-//			Field.Store.YES,Field.Index.NOT_ANALYZED);
-//		//index file path
-//		Field filePathField = new Field(LuceneConstants.FILE_PATH,
-//			file.getCanonicalPath(),
-//			Field.Store.YES,Field.Index.NOT_ANALYZED);
-//		document.add(contentField);
-//		document.add(fileNameField);
-//		document.add(filePathField);
-//		return document;
-//	}
-//	private void indexFile(File file) throws IOException
-//	{
-//		System.out.println("Indexing "+file.getCanonicalPath());
-//		Document document = getDocument(file);
-//		writer.addDocument(document);
-//	}
-//	public int createIndex(String dataDirPath, FileFilter filter) throws IOException
-//	{
-//		//get all files in the data directory
-//		File[] files = new File(dataDirPath).listFiles();
-//		for (File file : files)
-//		{
-//			if(!file.isDirectory() && !file.isHidden() && file.exists() && file.canRead() && filter.accept(file) )
-//			{
-//				indexFile(file);
-//			}
-//		}
-//		return writer.numDocs();
-//	}
+	public IndexWriter configurarIndexador(){
+		analizador = new Analyzer() {
+
+			@Override
+			protected TokenStreamComponents createComponents(String nombreCampo) {
+				TokenizerEspanol tokenizerEspanol = new TokenizerEspanol();
+				// Se asignan los stopwords al analizador
+				TokenStream filtroMinusculas = new LowerCaseFilter(tokenizerEspanol);
+				TokenStream filtroStopWords = new StopFilter(filtroMinusculas, stopWords);
+				// Se hace el stemming de acuerdo a la implementación Snowball del stemmer en español
+				SpanishStemmer stemmerEspanol = new SpanishStemmer();
+				TokenStream filtro = new SnowballFilter(filtroStopWords,stemmerEspanol);
+				return new TokenStreamComponents(tokenizerEspanol, filtro);
+			}
+		};
+		IndexWriterConfig configuracionIndice = new IndexWriterConfig(analizador);
+		Path rutaDirectorioIndice = Paths.get(".","/indices");
+		try {
+			Directory directorioIndice = FSDirectory.open(rutaDirectorioIndice);
+			writer = new IndexWriter(directorioIndice, configuracionIndice);
+			return writer;
+		}
+		catch (IOException e){
+			System.out.println("Hubo un problema al configurar el indexador");
+		}
+		return null;
+	}
+
+	public void indexarContenidos(String html)
+	{
+		Document DocumentoLucene = new Document();
+
+		org.jsoup.nodes.Document Html = Jsoup.parse(html);
+		// Se indexa el <body>
+		String HTML = Html.body().text();
+		IndexableField body = new TextField("texto",HTML, Field.Store.YES);
+
+		// Se indexa el <title>
+		HTML = Html.getElementsByTag("title").text();
+		IndexableField title = new TextField("titulo",HTML,Field.Store.YES);
+		// Se indexa los <h?>
+		StringBuilder encabezados = new StringBuilder();
+		encabezados.append(Html.getElementsByTag("h1").text());
+		encabezados.append(Html.getElementsByTag("h2").text());
+		encabezados.append(Html.getElementsByTag("h3").text());
+		encabezados.append(Html.getElementsByTag("h4").text());
+		encabezados.append(Html.getElementsByTag("h5").text());
+		encabezados.append(Html.getElementsByTag("h6").text());
+
+		IndexableField headers = new TextField("encab",encabezados.toString(),Field.Store.YES);
+
+		// Se indexa las <a>
+		HTML = Html.getElementsByTag("a").text();
+		IndexableField links = new TextField("ref",HTML,Field.Store.YES);
+
+
+		DocumentoLucene.add(headers);
+		DocumentoLucene.add(links);
+		DocumentoLucene.add(body);
+		DocumentoLucene.add(title);
+
+		try {
+			writer.addDocument(DocumentoLucene);
+		}
+		catch (IOException e) {
+			System.out.println("Hubo un error en la indexacion");
+		}
+	}
 }
