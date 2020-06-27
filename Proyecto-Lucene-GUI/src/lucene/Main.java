a21009adb406745d24ca01c2c0090496ceb7e2c5
Xfighter32 <austinhakanson2@gmail.com>
2020-06-26 22:54:32 -0600

    me falto el main xd

--- Proyecto-Lucene-GUI/src/lucene/Main.java
+++ Proyecto-Lucene-GUI/src/lucene/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         StackPane root = FXMLLoader.load(getClass().getResource("gui.fxml"));
         primaryStage.setTitle("Progra II RIT");
-        primaryStage.setScene(new Scene(root, 500, 400));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
